c01adbcd246401cb1e83e54e9e2144e68613ace7
analog-nico <nicolai.kamenzky@testrails.org>
2016-06-08 12:45:50 -0700

    feat: input validation

---
+++ test/spec/main.js
@@ -0,0 +1,111 @@
+/* jshint nonew:false */
+'use strict';
+
+var PinterestStrategy = require('../../').Strategy;
+
+
+describe('Passport-Pinterest', function () {
+
+    it('should initialize', function () {
+
+        expect(function () {
+
+            new PinterestStrategy();
+
+        }).to.throw('Please pass the options.');
+
+        expect(function () {
+
+            new PinterestStrategy({});
+
+        }).to.throw('Please pass the verify callback.');
+
+        expect(function () {
+
+            new PinterestStrategy({}, function () {});
+
+        }).to.throw();
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234'
+            }, function () {});
+
+        }).not.to.throw();
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                state: false
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.state');
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                state: ''
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.state');
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                state: 'abcd'
+            }, function () {});
+
+        }).not.to.throw();
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                authorizationURL: false
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.authorizationURL');
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                tokenURL: false
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.tokenURL');
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                scopeSeparator: false
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.scopeSeparator');
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                sessionKey: false
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.sessionKey');
+
+        expect(function () {
+
+            new PinterestStrategy({
+                clientID: '1234',
+                profileURL: false
+            }, function () {});
+
+        }).to.throw('Please pass a string to options.profileURL');
+
+    });
+
+});
